56474bab95b41cb4da2a0765204315bc07208a70
m7-malmir <m.malmir666@gmail.com>
2024-12-10 16:56:30 +0330

    work on variables

--- Ljs/try.js
+++ Ljs/try.js
@@ -2,4 +2,28 @@
 // console.error('this is an error');
 // console.warn('this is a warning');
 
- 
+ //var , let , const
+
+//  var age=29;
+// age=30;
+ //console.log(age);
+
+ // string, numbers, null, undefined
+//  const name='john';
+//  const age=30;
+//  const rating=4.5;
+//  const isCool=true;
+//  const x= null;
+//  const y=undefined;
+//  let z;
+
+//  console.log(typeof name);
+
+const name='john';
+const age=30;
+//concatenation
+console.log('my name is:'+name+' and my age is:'+age);
+
+//template string
+const Hi=`my name is ${name} and i am ${age} years old`;
+console.log(Hi);
