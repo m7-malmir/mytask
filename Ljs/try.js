a01f2ea652222ddd7c0dd0557ff9f3d9e6a42b9d
m7-malmir <m.malmir666@gmail.com>
2024-12-11 17:06:34 +0330

    some file edit and add for birthday

--- Ljs/try.js
+++ Ljs/try.js
@@ -19,11 +19,85 @@
 
 //  console.log(typeof name);
 
-const name='john';
-const age=30;
+// const name='john';
+// const age=30;
 //concatenation
-console.log('my name is:'+name+' and my age is:'+age);
+//console.log('my name is:'+name+' and my age is:'+age);
 
 //template string
-const Hi=`my name is ${name} and i am ${age} years old`;
-console.log(Hi);
+// const Hi=`my name is ${name} and i am ${age} years old`;
+// console.log(Hi);
+
+// const s='hello world';
+// const words='mohammad, ali, sara';
+// console.log(s.toUpperCase());
+// console.log(s.substring(0,6).toUpperCase());
+// console.log(words.split(','));
+
+//arrays
+/* variables that holds multiple value*/ 
+// const numbers=new Array(1,2,3,4);
+// console.log(numbers);
+// const fruits=['banana','apple','orange'];
+// fruits[5]='grapes';
+// fruits.push('mangos');
+// fruits.unshift('peanapple');
+// fruits.pop();
+// console.log(Array.isArray(fruits));
+// console.log(fruits.indexOf('grapes'));
+// console.log(fruits);
+
+// const person={
+//     firstName:'mohammad',
+//     lastName:'malmir',
+//     age:30,
+//     hobbies:['music','book','code'],
+//     address:{
+//         street:'50 main st',
+//         city: 'Navan',
+//         state: 'MA'
+//     }
+// }
+// console.log(person.firstName , person.lastName);
+//  console.log(person.address.city);
+//  const {firstName,lastName,address:{city}}=person;
+//person.email='m.malmir@gmail.com';
+// console.log(city);
+
+const todos=[
+    {
+        id:1,
+        text:'Take out trash',
+        isCompleted:true
+    },
+    {
+        id:2,
+        text:'get ticket ',
+        isCompleted:false
+    },
+    {
+        id:3,
+        text:'do the lundry',
+        isCompleted:false
+    }
+];
+// console.log(todos[1].text);
+
+// const todoJSON=JSON.stringify(todos);
+// console.log(todoJSON);
+
+
+// Loops
+// for(let i=0;i < 9;i++){
+//     console.log(`For loop number: ${i}`);
+// }
+// let i=0;
+// while(i<10){
+//     console.log(`while loop number: ${i}`);  
+//     i++;
+// }
+
+for (let i = 0; i < todos.length; i++) {
+   console.log(todos[i]);
+    
+}
