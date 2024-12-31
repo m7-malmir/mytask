f2c00366c21a3876ced41d20d0935ed85f515003
m7-malmir <m.malmir666@gmail.com>
2024-12-31 17:37:37 +0330

    edit car is edited

--- VRM/VAM/editcar-ready.js
+++ VRM/VAM/editcar-ready.js
@@ -7,179 +7,205 @@ var WithRole;
 var PersonnelNO;
 var html_;
 var is_printed = 0;
-var CarId; // تعریف متغیر CarId بدون مقداردهی اولیه
+var CarId; 
 
-$(function(){
-	$form = (function()
-	{
-		// دریافت CarId از dialogArguments
-		if(typeof dialogArguments !== "undefined" && "CarId" in dialogArguments) {
-			CarId = dialogArguments.CarId; // مقداردهی CarId از پارامترهای فرم دیگر
-		} else {
-			CarId = 12; // مقدار پیش‌فرض در صورت عدم ارسال از فرم دیگر
-		}
 
-		alert(JSON.stringify(CarId)); // نمایش CarId برای بررسی
-		var pk,
-			inEditMode = false,
-			primaryKeyName = "Id",
-			bindingSourceName = "BS_InsertCar",
-			readFromData = FormManager.readEntity,
+
+$(function() {
+    $form = (function() {
+        var pk,
+            inEditMode = false,
+            primaryKeyName = "Id",
+            bindingSourceName = "BS_InsertCar",
+			updateFromData = FormManager.updateEntity,
+            readFromData = FormManager.readEntity,
             insertFromData = FormManager.insertEntity;
-		
-		function init()
-		{
-			if(typeof dialogArguments !== "undefined")
-			{
-				if(primaryKeyName in dialogArguments)
-				{
-					pk = dialogArguments[primaryKeyName];
-					inEditMode = true;
-					readData();
-				}
-				if("FormParams" in dialogArguments)
-				{
-					pk = dialogArguments.FormParams;
-					inEditMode = true;
-					readData();
-				}
-			}
-			build();
-			createControls();
-			bindEvents();
-		}
-		
-		function build()
-		{
-			$("body").css({overflow: "hidden"}).attr({scroll: "no"});
-			$("#Form1").css({top: "0", left: "0", width: $(document).width() + "px", height: $(document).height() + "px"});
-		}
+        // دریافت CarId از dialogArguments
+        if (typeof dialogArguments !== "undefined" && "CarId" in dialogArguments) {
+            CarId = dialogArguments.CarId; // مقداردهی CarId از پارامترهای فرم دیگر
+			inEditMode = true;
+        }
+    	
+        function init() {
+            if (typeof dialogArguments !== "undefined") {
+            }
+            build();
+            createControls();
+            bindEvents();
+        }
 
-		function createControls()
-		{
-			Server.GetCurrentDate("dddd d MMMM yyyy", 'fa', true,
-			    function(data) {
-					$("#LabelControl12").text(data);
-			    },
-			    function(error) {
-			        $ErrorHandling.Error(error, " خطا در تاریخ جاری سیستم");
-			});
+        function build() {
+            $("body").css({ overflow: "hidden" }).attr({ scroll: "no" });
+            $("#Form1").css({ top: "0", left: "0", width: $(document).width() + "px", height: $(document).height() + "px" });
+        }
 
-			if(!inEditMode){
-				$("#PanelControl2").hide();
-				$("#LabelControl1").hide();
-				UserService.GetCurrentActor(true,
-					function(data){
-						hideLoading();
-						var xmlActor = $.xmlDOM(data);
-						currentActorId = xmlActor.find('actor').attr('pk');
-						var params = {Where: "Id = '" + CarId + "'"}; // استفاده از CarId
-						alert(JSON.stringify(CarId));
-						BS_InsertCar.Read(params, function(data) {
-							var dataXml = null;
-							if($.trim(data) != "")
-							{
-								dataXml = $.xmlDOM(data);
-								
-								// خواندن اطلاعات خودرو
-								CarName = dataXml.find("row:first").find(">col[name='CarName']").text();
-								ModelYear = dataXml.find("row:first").find(">col[name='ModelYear']").text();
-								Color = dataXml.find("row:first").find(">col[name='Color']").text();
-								SS = dataXml.find("row:first").find(">col[name='SS']").text();
-								CarNO = dataXml.find("row:first").find(">col[name='CarNO']").text();
-								Status = dataXml.find("row:first").find(">col[name='Status']").text();
-								
-								if (ServiceLocationId == "2")
-								{
-								    $("#ButtonControl13").show();
-								}
-								$("#CarName").val(CarName);
-								$("#CarModel").val(ModelYear);
-								$("#CarColor").val(Color);
-								$("#CarSS").val(SS);
-								$("#CarNo").val(CarNO);
-								$("#txtPosition").val(Status);
-								$("#CarSS").prop('disabled', false);
-							}
-						});
-					},
-					function(err){
-						hideLoading();
-						$ErrorHandling.Error(err,"خطا در سرویس getCurrentActor");
-					}
-				);
-			} else {
-				// کد مربوط به حالت ویرایش
-				readData();
-			}
-		}
+        function createControls() {
+            Server.GetCurrentDate("dddd d MMMM yyyy", 'fa', true,
+                function(data) {
+                    $("#LabelControl12").text(data);
+                },
+                function(error) {
+                    $ErrorHandling.Error(error, " خطا در تاریخ جاری سیستم");
+                });
 
-		function bindEvents() {
-			$("#saveButton").click(function() { // فرض بر این است که دکمه ذخیره با id "saveButton" مشخص شده است
-				saveData();
-			});
-		}
+            if (!inEditMode) {
+                $("#PanelControl2").hide();
+                $("#LabelControl1").hide();
+                UserService.GetCurrentActor(true,
+                    function(data) {
+                        hideLoading();
+                        var xmlActor = $.xmlDOM(data);
+                        currentActorId = xmlActor.find('actor').attr('pk');
+                        var params = { Where: "Id = '" + CarId + "'" }; // استفاده از CarId
+                        //alert(JSON.stringify(CarId));
+                        BS_InsertCar.Read(params, function(data) {
+                            var dataXml = null;
+                            if ($.trim(data) != "") {
+                                dataXml = $.xmlDOM(data);
 
-		function readData() {
-			// کد مربوط به خواندن داده‌ها
-		}
+                                // خواندن اطلاعات خودرو
+                                CarName = dataXml.find("row:first").find(">col[name='CarName']").text();
+                                ModelYear = dataXml.find("row:first").find(">col[name='ModelYear']").text();
+                                Color = dataXml.find("row:first").find(">col[name='Color']").text();
+                                SS = dataXml.find("row:first").find(">col[name='SS']").text();
+                                CarNO = dataXml.find("row:first").find(">col[name='CarNO']").text();
+                                Status = dataXml.find("row:first").find(">col[name='Status']").text();
 
-		function getPK() {
-			return pk;
-		}
+                                if (ServiceLocationId == "2") {
+                                    $("#ButtonControl13").show();
+                                }
+                                $("#CarName").val(CarName);
+                                $("#CarModel").val(ModelYear);
+                                $("#CarColor").val(Color);
+                                $("#CarSS").val(SS);
+                                $("#CarNo").val(CarNO);
+                                $("#txtPosition").val(Status);
+                                $("#CarSS").prop('disabled', false);
+                            }
+                        });
+                    },
+                    function(err) {
+                        hideLoading();
+                        $ErrorHandling.Error(err, "خطا در سرویس getCurrentActor");
+                    }
+                );
+            } else {
+                // کد مربوط به حالت ویرایش
+                readData();
+            }
+        }
 
-		function isInEditMode() {
-			return inEditMode;
-		}
+        function bindEvents() {
+            $("#saveButton").click(function() { // فرض بر این است که دکمه ذخیره با id "saveButton" مشخص شده است
+                saveData();
+            });
+        }
 
-		function saveData() {
-			var params = {
-				CarName: $("#CarName").val(),
-				ModelYear: $("#CarModel").val(),
-				Color: $("#CarColor").val(),
-				SS: $("#CarSS").val(),
-				CarNO: $("#CarNo").val(),
-				Status: $("#txtPosition").val(),
-				CreatorActor_ID: currentActorId,
-				PersonnelNO: PersonnelNO,
-				ServiceLocationId: ServiceLocationId,
-				RequestType: RequestType,
-				WithRole: WithRole
-			};
+        function readData() {
+            showLoading();
+			alert(JSON.stringify({ Where: primaryKeyName + " = " + CarId}));
+            readFromData({ Where: primaryKeyName + " = " + CarId },
+                function(dataXml) {
+                    hideLoading();
+                    $.setFormDataValues(bindingSourceName, dataXml);
+                },
+                function(err) {
+                    hideLoading();
+                    alert(err);
+                }
+            );
+        }
 
-			insertFromData(params, function(dataXml) {
-				// پردازش موفقیت‌آمیز
-				alert("اطلاعات با موفقیت ذخیره شد.");
-				closeWindow({OK: true, Result: null}); // بستن پنجره
-			}, function(err) {
-				// پردازش خطا
-				alert("خطا در ذخیره اطلاعات: " + err);
-			});
-		}
+        function getPK() {
+            return pk;
+        }
 
-		function insertData(callback) {
-			// کد مربوط به درج داده‌ها
-		}
+        function isInEditMode() {
+            return inEditMode;
+        }
+        function saveData() {
+            validateForm(
+                function() {
+                    if (inEditMode) {
+						updateData();
+                    } else {
+                        insertData();
+                    }
+                },
+                function() {
+                    $.alert("لطفا موارد اجباری را پر کنید", "", "rtl",
+                        function() { }
+                    );
+                }
+            );
+        }
 
+        function insertData() {
+            showLoading();
+            var params = $.getFormDataValues(bindingSourceName);
+            if (params.status == 'فعال') {
+                params.Status = 1;
+            } else {
+                params.Status = 0;
+            }
+            insertFromData(params,
+                function(dataXml) {
+                    pk = dataXml.find("row:first").find(">col[name='" + primaryKeyName + "']").text();
+                    closeWindow({ OK: true, Result: pk });
+                },
+                function(err) {
+                    hideLoading();
+                    alert(err);
+                }
+            );
+        }
 		function updateData(callback) {
-			// کد مربوط به به‌روزرسانی داده‌ها
-		}
-		
-		function deleteData(callback) {
-			// کد مربوط به حذف داده‌ها
+		    showLoading();
+			var params = {};
+			params = $.extend(params, { Where: primaryKeyName + " = " + CarId });
+			updateFromData(params,
+				function(data)
+				{
+					Office.Inbox.setResponse(dialogArguments.WorkItem,1, ""
+					    , function(data)
+					    { 
+					        closeWindow({OK:true, Result:CarId });
+					    }, function(err){ throw Error(err); }
+					);
+				},
+				function(err)
+				{
+					hideLoading();
+					alert(err);
+				}
+			);
+			
 		}
 
-		function validateForm(onSuccess, onError) {
-			// کد مربوط به اعتبارسنجی فرم
-		}
-		
-		return {
-			init: init,
-			getPK: getPK,
-			isInEditMode: isInEditMode,
-			validateForm: validateForm,
-			saveData: saveData
-		};
-	}());
-	$form.init();
+        function deleteData(callback) {
+            // کد مربوط به حذف داده‌ها
+        }
+
+        function validateForm(onSuccess, onError) {
+            try {
+                $("[role]").validateData(true);
+                if ($.isFunction(onSuccess)) {
+                    onSuccess();
+                }
+            } catch (e) {
+                if ($.isFunction(onError)) {
+                    onError();
+                }
+            }
+        }
+
+        return {
+            init: init,
+            getPK: getPK,
+            isInEditMode: isInEditMode,
+            saveData: saveData,
+            deleteData: deleteData
+        };
+    }());
+    $form.init();
 });
