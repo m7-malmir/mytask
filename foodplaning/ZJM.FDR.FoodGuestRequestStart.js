04b596609d335b506aa0af4298afb9a769aa01e7
m7-malmir <m.malmir666@gmail.com>
2025-04-05 15:27:14 +0330

    work on guests start

--- foodplaning/ZJM.FDR.FoodGuestRequestStart.js
+++ foodplaning/ZJM.FDR.FoodGuestRequestStart.js
@@ -1,75 +1,154 @@
-//#region 
-var tblMain = null;
-
-$(function () {
-    tblMain = (function () {
-        var element = $("#tblFood");
-        init();
-        function init() {
-            bindEvents();
-        }
-
-        function bindEvents() {
-            $("#btnRegisterGuests").click(function () {
-                var firstName = $("#txtFirstName").val().trim();
-                var lastName = $("#txtLastName").val().trim();
-                
-                if (firstName === "" || lastName === "") {
-                    alert("لطفاً نام و نام خانوادگی را وارد کنید.");
-                    return;
-                }
-
-                // شمارش تعداد ردیف‌های داده‌ای برای تنظیم شناسه از 1
-                var newId = element.find("tr.row-data").length + 1;
-
-                var rowInfo = {
-                    RowNumber: newId, // شماره ردیف
-                    Id: newId, // شناسه که از ۱ شروع می‌شود
-                    FirstName: firstName,
-                    LastName: lastName,
-                    ReservationCode: Math.floor(Math.random() * 10000) // کد رزرو تصادفی
-                };
-
-                addRow(rowInfo);
-            });
-
-            element.on("click", ".delete", function () {
-                $(this).closest("tr").remove();
-                updateRowNumbers(); // به‌روزرسانی شماره‌ها بعد از حذف
-            });
-        }
-
-        function addRow(rowInfo) {
-            var tempRow = element.find("tr.row-template").clone();
-            tempRow.show().removeClass("row-template").addClass("row-data");
-
-            var index = 0;
-            tempRow.find("td:eq(" + index++ + ")").text(rowInfo.RowNumber); // شماره ردیف
-            tempRow.find("td:eq(" + index++ + ")").text(rowInfo.Id); // شناسه
-
-            var imgDelete = $("<img/>", {
-                src: "Images/delete.png",
-                title: "حذف",
-                class: "delete",
-                css: { cursor: "pointer" }
-            });
-
-            tempRow.find("td:eq(" + index++ + ")").empty().append(imgDelete); // حذف
-            tempRow.find("td:eq(" + index++ + ")").text(rowInfo.FirstName); // نام
-            tempRow.find("td:eq(" + index++ + ")").text(rowInfo.LastName); // نام خانوادگی
-            tempRow.find("td:eq(" + index++ + ")").text(rowInfo.ReservationCode); // کد رزرو
-
-            element.find("tr.row-template").before(tempRow);
-        }
-
-        function updateRowNumbers() {
-            element.find("tr.row-data").each(function (index, row) {
-                $(row).find("td:eq(0)").text(index + 1); // شماره ردیف
-                $(row).find("td:eq(1)").text(index + 1); // شناسه
-            });
-        }
-
-    })();
+var $form;
+var currentActorId;
+var ProcessStatus;
+var currentPersonnelNO;
+var FoodId;
+$(function(){
+	$form = (function()
+	{ 
+		var pk,
+			inEditMode = false,
+			primaryKeyName = "Id",
+			bindingSourceName = "",
+			insertFromData = FormManager.insertEntity,
+			readRows = FormManager.readEntityّSPFoodView;
+		//******************************************************************************************************	
+		function init()
+		{ 
+			build();
+			createControls();
+			bindEvents();
+		}
+        //******************************************************************************************************
+		function build()
+		{
+			//اگر بخواهیم استیل دهی خاصی داشته باشیم در این متد اعمال می شود
+			$("body").css({overflow: "hidden"}).attr({scroll: "no"});
+		}
+		//******************************************************************************************************
+		//مقداردهی به المان ها در هر دو حالت ویرایش و ایجاد
+		function createControls()
+		{	
+			showLoading();
+			$("#btnEdit").css('display','none');
+			UserService.GetCurrentActor(true,
+				function(data){
+						hideLoading();
+						var xmlActor = $.xmlDOM(data);
+						currentActorId = xmlActor.find('actor').attr('pk');
+					},
+				function(err){
+					hideLoading();
+					$ErrorHandling.Erro(err,"خطا در سرویس getCurrentActor");
+				}
+			);
+		}
+		//******************************************************************************************************
+		// تمام ایونت های مربوط به یک المان یا خود فرم در این متد نوشته می شوند
+		// مانند ماوس هاور و ...
+		function bindEvents()
+		{
+		}
+		//******************************************************************************************************
+		function readData()
+		{
+		}
+		//******************************************************************************************************
+		// برای دریافت شناسه فرایند بعد از ایجاد و یا در ویرایش استفاده می شود
+		// برای دریافت در کد سایر المان ها از ایسن متد استفاده می کنیم
+		function getPK()
+		{
+			return pk;
+		}
+		//******************************************************************************************************
+		// برای دریافت شناسه فرایند بعد از ایجاد و یا در ویرایش استفاده می شود
+		// برای دریافت در کد سایر المان ها از ایسن متد استفاده می کنیم
+		function isInEditMode()
+		{
+			return inEditMode;
+		}
+		//******************************************************************************************************
+		function saveData(callback)
+		{
+			if(inEditMode)
+			{
+				updateData(callback);
+			}
+			else
+			{
+				insertData(callback);
+			}
+		}
+		//******************************************************************************************************
+		function insertData(callback)
+		{
+			showLoading();
+			var params = $.getFormDataValues(bindingSourceName);
+			params.CreatorActor_ID = currentActorId;
+			insertFromData(params,
+				function()
+				{
+					alert(JSON.stringify(pk));
+					WorkflowService.RunWorkflow("ZJM.FDR.FoodGuestsReservation",
+					    '<Content><Id>'+pk+'</Id></Content>',
+					    true,
+					    function(data)
+					    {
+					        $.alert("درخواست شما با موفقیت ارسال شد.","","rtl",function(){
+								hideLoading();
+					        	closeWindow({OK:true, Result:null});
+							});				
+					    }
+					    ,function(err)
+					    {
+					        alert('مشکلی در شروع فرآیند به وجود آمده. '+err);
+					        hideLoading();
+					    }
+					);
+					myHideLoading();
+					if($.isFunction(callback))
+					{
+						callback();
+					}
+				}
+			);	    
+		}
+		//******************************************************************************************************
+		function updateData(callback)
+		{
+		}
+		//******************************************************************************************************
+		function deleteData(callback)
+		{
+			// اگر بخواهیم خود اینستنس فرایند را حذف کنیم کد فراخوانی حذف از فرم منیجر را اینجا می نویسیم
+		}
+		//******************************************************************************************************
+		function validateForm(onSuccess, onError)
+		{
+			try
+			{
+				$("[role]").validateData(true);
+				if($.isFunction(onSuccess))
+				{
+					onSuccess();
+				}
+			}
+			catch(e)
+			{
+				if($.isFunction(onError))
+				{
+					onError();
+				}
+			}
+		}
+		//******************************************************************************************************
+		return {
+			init: init,
+			getPK: getPK,
+			isInEditMode: isInEditMode,
+			validateForm: validateForm,
+			saveData: saveData
+		};
+	}());
+	$form.init();
 });
-
-//#endregion
