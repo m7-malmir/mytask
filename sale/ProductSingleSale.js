7461c0e61256b8db0dcdc190a20383641aa575b9
m7-malmir <m.malmir666@gmail.com>
2025-06-03 16:42:12 +0330

    files edited

--- sale/ProductSingleSale.js
+++ sale/ProductSingleSale.js
@@ -533,231 +533,113 @@ function getTotalPrice() {
 //#endregion
 
 //#region form manager
-var $form;
+var FormManager = {
 
-//---------------------------------
-// Global Variables For UserInfo
-//---------------------------------
-var currentusername;
-var currentPersonnelNO;
-var currentUserCompanyId;
-var nationalCode;
-var currentUserfirstname;
-var currentUserlastname;
-var birthday;
-var email;
-var employmentDate;
-var rankTitle;
-var leaveDate;
-var gender;
-
-var currentActorId;
-var selectedValue;
-//---------------------------------
-var CreditBalance;
-var ProcessStatus;
-var currentUserId;
-var Discount;
-$(function(){
-	$form = (function()
-	{ 
-		var pk,
-			isInTestMode = false;
-			inEditMode = false,
-			primaryKeyName = "Id",
-			bindingSourceName = "",
-			readBrandName=FormManager.ReadBrandName,
-			ReadPersonnelCredit=FormManager.ReadPersonnelCredit,
-			readEmployeeInfo = UserHelpes.readEmployeeInfo;
-		//******************************************************************************************************	
-		function init()
-		{ 
-			build();
-			createControls();
-			bindEvents();
-		}
-        //******************************************************************************************************
-		function build()
-		{
-			//اگر بخواهیم استیل دهی خاصی داشته باشیم در این متد اعمال می شود
-			$("body").css({overflow: "hidden"}).attr({scroll: "no"});
-		}
-		//******************************************************************************************************
-		//مقداردهی به المان ها در هر دو حالت ویرایش و ایجاد
-		function createControls()
-		{
-			//-----------------------------------
-			//	Get Test Mode Value
-			//-----------------------------------
-			try {
-				const parentUrl = window.parent?.location?.href;
-				const url = new URL(parentUrl);
-		   	 isInTestMode = url.searchParams.get("icantestmode") === "1";
-			  }
-			  catch (e) {
-			    console.warn("Cannot reach parent document:", e);
-			    isInTestMode = false;
-			  }
-			/******************************************************************************/
-			var jsonParams = {}; // پارامترهای مورد نیاز برای خواندن داده‌ها
-
-		    readBrandName(jsonParams, function(brandOptions) {
-		        // پر کردن سلکتور با گزینه‌ها
-		        $('#CmbBrandFilter').html(brandOptions);
-		    }, function(error) {
-		        console.error("Error fetching brand data:", error);
-		    });
-			/******************************************************************************/
+	//******************************************************************************************************
+	readEntityGoodsCatalogue: function(jsonParams, onSuccess, onError)
+	{
+	  BS_vw_IS_GoodsCatalogue.Read(jsonParams
+	       , function(data)
+	       {
+	           var list = [];
+	           var xmlvar = $.xmlDOM(data);
+	           xmlvar.find("row").each(
+	               function()
+	               { 
+	                   list.push
+	                   ({
+			                 GoodsId: $(this).find("col[name='GoodsId']").text(),
+			                 GoodsCode: $(this).find("col[name='GoodsCode']").text(),
+			                 GoodsName: $(this).find("col[name='GoodsName']").text(),
+			                 LogicalQty: $(this).find("col[name='LogicalQty']").text(),
+						     Price: $(this).find("col[name='Price']").text(),
+			                 BrandName: $(this).find("col[name='BrandName']").text()
+	                   });
+	               }
+	           );
+	           if($.isFunction(onSuccess))
+	           {
+	               onSuccess(list);
+	           
+	           }
+	       }, onError
+	   );
+	},
+/*********************************************************************************************************/
 	
-			showLoading();
-			UserService.GetCurrentUser(true,
-				function(data){
-						hideLoading(); 
-						FormManager.ReadPersonnelCredit({},
-							function(list)
-							{
-								CreditBalance=list[0].RemainCredit;
-								DiscountPercent=list[0].DiscountPercent;
-								LimitDiscountPercent=list[0].LimitDiscountPercent;
-								
-								$("#txtCreditBalance").val(CreditBalance);	
-								
-								if(list[0].CancelCredit=='false'){
-									Discount=DiscountPercent;	
-								}else{
-									Discount=LimitDiscountPercent;
-								}
-								
-							},
-							function(error)
-						    {
-						        alert('خطایی در سیستم رخ داده است: '+error.erroMessage);
-						        myHideLoading();
-								return;
-						    }
-						);
-						
-						var xml = $.xmlDOM(data);
-						currentUserId = xml.find("user > id").text().trim();
-				        currentusername = xml.find("user > username").text().trim();
-				        currentUserfirstname = xml.find("user > firstname").text().trim();
-				        currentUserlastname = xml.find("user > lastname").text().trim();
-				        currentActorId = xml.find("actor").attr("pk");
-						$("#txtFullName").val(currentUserfirstname+' '+currentUserlastname);
-						$("#txtPersonnelNO").val(currentusername);
-						tblMain.refresh();
-						readEmployeeInfo(currentusername,
-			                function(list)
-			                {
-								if(list.length > 1){
-									ErrorMessage("خطا در دیافت اطلاعات کاربری، لطفاً به پشتیبانی سیستم اطلاع رسانی نمایید",list);
-								}
-								else {
-									currentUserCompanyId = list[0]["CurrentUserCompanyId"];
-									
-								}
-								myHideLoading();
-			                },
-			                function(error)
-			                {
-								myHideLoading();
-			                    alert(error);
-			                }
-			            );
-					},
-				function(err){
-					hideLoading();
-					$ErrorHandling.Erro(err,"خطا در سرویس getCurrentActor");
-				}
-			);
-			
+	ReadBrandName: function(jsonParams, onSuccess, onError) {
+	    SP_vw_IS_GoodsCatalogue_Brand.Execute(jsonParams, function(data) {
+	        var xmlvar = $.xmlDOM(data);
+	        var brandOptions = '';
+	
+	        xmlvar.find("row").each(function() {
+	            var BrandRef = $(this).find(">col[name='BrandRef']").text();
+	            var BrandName = $(this).find(">col[name='BrandName']").text();
+	            brandOptions += '<option value="' + BrandRef + '">' + BrandName + '</option>';
+	        });
+	
+	        // اگر onSuccess یک تابع باشد، آن را فراخوانی کنید و گزینه‌ها را به آن بفرستید
+	        if ($.isFunction(onSuccess)) {
+	            onSuccess(brandOptions);
+	        }
+	    }, onError);
+	},
+		//******************************************************************************************************
+	ReadPersonnelCredit: function(jsonParams, onSuccess, onError)
+	{
+	  BS_HRPersonnelCredit.Read(jsonParams
+	       , function(data)
+	       {
+	           var list = [];
+	           var xmlvar = $.xmlDOM(data);
+	           xmlvar.find("row").each(
+	               function()
+	               { 
+	                   list.push
+	                   ({
+			                 Id: $(this).find("col[name='Id']").text(),
+			                 Credit: $(this).find("col[name='Credit']").text(),
+			                 RemainCredit: $(this).find("col[name='RemainCredit']").text(),
+						 	CancelCredit: $(this).find("col[name='CancelCredit']").text(),
+						     DiscountPercent: $(this).find("col[name='DiscountPercent']").text(),
+						 	LimitDiscountPercent: $(this).find("col[name='LimitDiscountPercent']").text()
+	                   });
+	               }
+	           );
+	           if($.isFunction(onSuccess))
+	           {
+	               onSuccess(list);
+	           
+	           }
+	       }, onError
+	   );
+	},
+/*********************************************************************************************************/
 
-						
-		}
-		//******************************************************************************************************
-		// تمام ایونت های مربوط به یک المان یا خود فرم در این متد نوشته می شوند
-		// مانند ماوس هاور و ...
-		function bindEvents()
-		{
-		}
-		//******************************************************************************************************
-		function readData()
-		{
-		}
-		//******************************************************************************************************
-		// برای دریافت شناسه فرایند بعد از ایجاد و یا در ویرایش استفاده می شود
-		// برای دریافت در کد سایر المان ها از ایسن متد استفاده می کنیم
-		function getPK()
-		{
-			return pk;
-		}
-		//******************************************************************************************************
-		// برای دریافت شناسه فرایند بعد از ایجاد و یا در ویرایش استفاده می شود
-		// برای دریافت در کد سایر المان ها از ایسن متد استفاده می کنیم
-		function isInEditMode()
-		{
-			return inEditMode;
-		}
-		//******************************************************************************************************
-		// برای دریافت شناسه فرایند بعد از ایجاد و یا در ویرایش استفاده می شود
-		// برای دریافت در کد سایر المان ها از ایسن متد استفاده می کنیم
-		function isInEditMode()
-		{
-			return inEditMode;
-		}
-		//******************************************************************************************************
-		function saveData(callback)
-		{
-			validateForm(
-				function()
-				{
-					if(inEditMode)
-					{
-						updateData(callback);
-					}
-					else
-					{
-						insertData(callback);
-					}
-				},
-				function(errorMessage) {
-		            $.alert(errorMessage || "لطفا موارد اجباری را تکمیل نمایید.", "", "rtl",
-		                function() {}
-		            );
-		        }
-			);
-		}
-		//******************************************************************************************************
-		function insertData(callback) {		    
-		}
-		//******************************************************************************************************
-		function updateData(callback)
-		{
-		}
-		//******************************************************************************************************
-		function deleteData(callback)
-		{
-			// اگر بخواهیم خود اینستنس فرایند را حذف کنیم کد فراخوانی حذف از فرم منیجر را اینجا می نویسیم
-		}
-		//******************************************************************************************************
-		function validateForm(onSuccess, onError)
-		{
-		}
-		//******************************************************************************************************
-		return {
-			init: init,
-			getPK: getPK,
-			isInEditMode: isInEditMode,
-			validateForm: validateForm,
-			saveData: saveData
-		};
-	}());
-	$form.init();
-});
+    RetailPersonnelOrder: function(jsonParams, onSuccess, onError) 
+	{
+        SP_RetailPersonnelOrder.Execute(jsonParams, function(data) {
+            var list = [];
+            var xmlvar = $.xmlDOM(data);
+            xmlvar.find("row").each(function() {
+                list.push({
+                    Result: $(this).find("col[name='res']").text()
+                });
+            });
+            if ($.isFunction(onSuccess)) {
+                onSuccess(list);
+            }
+        }, function(error) {
+            if ($.isFunction(onError)) {
+                onError(error);
+            }
+        });
+    }
+};
 //#endregion
 
 //#region btnRegister.js
- $("#btnRegister").click(function(){
+$("#btnRegister").click(function(){
 var jsonArray = [];
 
 // جمع‌آوری داده‌ها از جدول
