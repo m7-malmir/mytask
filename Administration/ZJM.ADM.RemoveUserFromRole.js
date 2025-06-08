440ac16bdc63cf2a28fd0b37f1e44fb76de30c19
m7-malmir <m.malmir666@gmail.com>
2025-06-08 16:43:44 +0330

    some file add

---
+++ Administration/ZJM.ADM.RemoveUserFromRole.js
@@ -0,0 +1,145 @@
+//#region 
+var $form;
+
+//---------------------------------
+// Global Variables For UserInfo
+//---------------------------------
+var currentusername;
+var currentPersonnelNO;
+var currentUserCompanyId;
+var nationalCode;
+var currentUserfirstname;
+var currentUserlastname;
+var birthday;
+var email;
+var employmentDate;
+var rankTitle;
+var leaveDate;
+var gender;
+
+var currentActorId;
+var selectedValue;
+//---------------------------------
+var GoodsHavePrice;
+var IS_Goods_ID;
+var CreditBalance;
+var ProcessStatus;
+$(function(){
+	$form = (function()
+	{ 
+		var pk,
+			isInTestMode = false;
+			inEditMode = false,
+			primaryKeyName = "Id",
+			bindingSourceName = "",
+			readEmployeeInfo = UserHelpes.readEmployeeInfo;
+		//******************************************************************************************************	
+		function init()
+		{ 
+			build();
+			createControls();
+			bindEvents();
+		}
+        //******************************************************************************************************
+		function build()
+		{
+			//اگر بخواهیم استیل دهی خاصی داشته باشیم در این متد اعمال می شود
+			$("body").css({overflow: "hidden"}).attr({scroll: "no"});
+		}
+		//******************************************************************************************************
+		//مقداردهی به المان ها در هر دو حالت ویرایش و ایجاد
+		function createControls()
+		{			
+		  	 UserService.GetCurrentUser(true,
+				function(data){
+						hideLoading(); 
+						var xml = $.xmlDOM(data);
+					
+					},
+				function(err){
+					hideLoading();
+					$ErrorHandling.Erro(err,"خطا در سرویس getCurrentActor");
+				}
+			);
+		}
+		//******************************************************************************************************
+		// تمام ایونت های مربوط به یک المان یا خود فرم در این متد نوشته می شوند
+		// مانند ماوس هاور و ...
+		function bindEvents()
+		{
+		}
+		//******************************************************************************************************
+		function readData()
+		{
+		}
+		//******************************************************************************************************
+		// برای دریافت شناسه فرایند بعد از ایجاد و یا در ویرایش استفاده می شود
+		// برای دریافت در کد سایر المان ها از ایسن متد استفاده می کنیم
+		function getPK()
+		{
+			return pk;
+		}
+		//******************************************************************************************************
+		// برای دریافت شناسه فرایند بعد از ایجاد و یا در ویرایش استفاده می شود
+		// برای دریافت در کد سایر المان ها از ایسن متد استفاده می کنیم
+		function isInEditMode()
+		{
+			return inEditMode;
+		}
+		//******************************************************************************************************
+		// برای دریافت شناسه فرایند بعد از ایجاد و یا در ویرایش استفاده می شود
+		// برای دریافت در کد سایر المان ها از ایسن متد استفاده می کنیم
+		function isInEditMode()
+		{
+			return inEditMode;
+		}
+		//******************************************************************************************************
+		function saveData(callback)
+		{
+			validateForm(
+				function()
+				{
+					if(inEditMode)
+					{
+						updateData(callback);
+					}
+					else
+					{
+						insertData(callback);
+					}
+				},
+				function(errorMessage) {
+		            $.alert(errorMessage || "لطفا موارد اجباری را تکمیل نمایید.", "", "rtl",
+		                function() {}
+		            );
+		        }
+			);
+		}
+		//******************************************************************************************************
+		function insertData(callback) {		    
+		}
+		//******************************************************************************************************
+		function updateData(callback)
+		{
+		}
+		//******************************************************************************************************
+		function deleteData(callback)
+		{
+			// اگر بخواهیم خود اینستنس فرایند را حذف کنیم کد فراخوانی حذف از فرم منیجر را اینجا می نویسیم
+		}
+		//******************************************************************************************************
+		function validateForm(onSuccess, onError)
+		{
+		}
+		//******************************************************************************************************
+		return {
+			init: init,
+			getPK: getPK,
+			isInEditMode: isInEditMode,
+			validateForm: validateForm,
+			saveData: saveData
+		};
+	}());
+	$form.init();
+});
+//#endregion
