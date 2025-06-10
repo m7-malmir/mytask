3b3fc7d650333475551ce1abb0518d4b898231e0
m7-malmir <m.malmir666@gmail.com>
2025-06-10 16:12:50 +0330

    some file add

--- sale/ZJM.PSE.ProductWholeOrder.js
+++ sale/ZJM.PSE.ProductWholeOrder.js
@@ -1,5 +1,222 @@
 //#region js.ready
+var $form;
 
+//---------------------------------
+// Global Variables For UserInfo
+//---------------------------------
+var currentusername;
+var currentPersonnelNO;
+var currentUserCompanyId;
+var nationalCode;
+var currentUserfirstname;
+var currentUserlastname;
+var birthday;
+var email;
+var employmentDate;
+var rankTitle;
+var leaveDate;
+var gender;
+
+var currentActorId;
+var selectedValue;
+//---------------------------------
+var CreditBalance;
+var ProcessStatus;
+var currentUserId;
+var Discount;
+$(function(){
+	$form = (function()
+	{ 
+		var pk,
+			isInTestMode = false;
+			inEditMode = false,
+			primaryKeyName = "Id",
+			bindingSourceName = "",
+			readBrandName=FormManager.ReadBrandName,
+			ReadPersonnelCredit=FormManager.ReadPersonnelCredit,
+			readEmployeeInfo = UserHelpes.readEmployeeInfo;
+		//******************************************************************************************************	
+		function init()
+		{ 
+			build();
+			createControls();
+			bindEvents();
+		}
+        //******************************************************************************************************
+		function build()
+		{
+			//اگر بخواهیم استیل دهی خاصی داشته باشیم در این متد اعمال می شود
+			$("body").css({overflow: "hidden"}).attr({scroll: "no"});
+			var jsonParams = {}; // پارامترهای مورد نیاز برای خواندن داده‌ها
+		    readBrandName(jsonParams, function(brandOptions) {
+		        $('#CmbBrandFilter').html(brandOptions);
+		    	}, function(error) {
+		        console.error("Error fetching brand data:", error);
+		    });
+		}
+		//******************************************************************************************************
+		//مقداردهی به المان ها در هر دو حالت ویرایش و ایجاد
+		function createControls()
+		{
+			/******************************************************************************/
+	
+			showLoading();
+			UserService.GetCurrentUser(true,
+				function(data){
+						hideLoading(); 
+						FormManager.ReadPersonnelCredit({},
+							function(list)
+							{
+								CreditBalance=list[0].RemainCredit;
+								DiscountPercent=list[0].DiscountPercent;
+								LimitDiscountPercent=list[0].LimitDiscountPercent;
+								
+								$("#txtCreditBalance").val(CreditBalance);	
+								
+								if(list[0].CancelCredit=='false'){
+									Discount=DiscountPercent;	
+								}else{
+									Discount=LimitDiscountPercent;
+								}
+								
+							},
+							function(error)
+						    {
+						        alert('خطایی در سیستم رخ داده است: '+error.erroMessage);
+						        myHideLoading();
+								return;
+						    }
+						);
+						
+						var xml = $.xmlDOM(data);
+					//alert(JSON.stringify(data));
+						currentUserId = xml.find("user > id").text().trim();
+				        currentusername = xml.find("user > username").text().trim();
+				        currentUserfirstname = xml.find("user > firstname").text().trim();
+				        currentUserlastname = xml.find("user > lastname").text().trim();
+				        currentActorId = xml.find("actor").attr("pk");
+						$("#txtFullName").val(currentUserfirstname+' '+currentUserlastname);
+						$("#txtPersonnelNO").val(currentusername);
+						tblMain.refresh();
+					
+						readEmployeeInfo(currentusername,
+			                function(list)
+			                {
+								if(list.length > 1 ){
+									$.alert("خطا در دیافت اطلاعات کاربری، لطفاً به پشتیبانی سیستم اطلاع رسانی نمایید","","rtl",function(){
+										hideLoading();
+							        	closeWindow({OK:true, Result:null});
+									});
+								}
+								else {
+									if(list[0]["DCId"]!=0){
+										$.alert("این فرآیند فقط برای پرسنل دفتر مرکزی فعال می باشد.","","rtl",function(){
+											hideLoading();
+								        	closeWindow({OK:true, Result:null});
+										});
+									}
+									currentUserCompanyId = list[0]["CurrentUserCompanyId"];
+								}
+								myHideLoading();
+			                },
+			                function(error)
+			                {
+								myHideLoading();
+			                    alert(error);
+			                }
+			            );
+										
+					},
+				function(err){
+					hideLoading();
+					$ErrorHandling.Erro(err,"خطا در سرویس getCurrentActor");
+				}
+			
+			);
+			
+
+						
+		}
+		//******************************************************************************************************
+		// تمام ایونت های مربوط به یک المان یا خود فرم در این متد نوشته می شوند
+		// مانند ماوس هاور و ...
+		function bindEvents()
+		{
+		}
+		//******************************************************************************************************
+		function readData()
+		{
+		}
+		//******************************************************************************************************
+		// برای دریافت شناسه فرایند بعد از ایجاد و یا در ویرایش استفاده می شود
+		// برای دریافت در کد سایر المان ها از ایسن متد استفاده می کنیم
+		function getPK()
+		{
+			return pk;
+		}
+		//******************************************************************************************************
+		// برای دریافت شناسه فرایند بعد از ایجاد و یا در ویرایش استفاده می شود
+		// برای دریافت در کد سایر المان ها از ایسن متد استفاده می کنیم
+		function isInEditMode()
+		{
+			return inEditMode;
+		}
+		//******************************************************************************************************
+		// برای دریافت شناسه فرایند بعد از ایجاد و یا در ویرایش استفاده می شود
+		// برای دریافت در کد سایر المان ها از ایسن متد استفاده می کنیم
+		function isInEditMode()
+		{
+			return inEditMode;
+		}
+		//******************************************************************************************************
+		function saveData(callback)
+		{
+			validateForm(
+				function()
+				{
+					if(inEditMode)
+					{
+						updateData(callback);
+					}
+					else
+					{
+						insertData(callback);
+					}
+				},
+				function(errorMessage) {
+		            $.alert(errorMessage || "لطفا موارد اجباری را تکمیل نمایید.", "", "rtl",
+		                function() {}
+		            );
+		        }
+			);
+		}
+		//******************************************************************************************************
+		function insertData(callback) {		    
+		}
+		//******************************************************************************************************
+		function updateData(callback)
+		{
+		}
+		//******************************************************************************************************
+		function deleteData(callback)
+		{
+			// اگر بخواهیم خود اینستنس فرایند را حذف کنیم کد فراخوانی حذف از فرم منیجر را اینجا می نویسیم
+		}
+		//******************************************************************************************************
+		function validateForm(onSuccess, onError)
+		{
+		}
+		//******************************************************************************************************
+		return {
+			init: init,
+			getPK: getPK,
+			isInEditMode: isInEditMode,
+			validateForm: validateForm,
+			saveData: saveData
+		};
+	}());
+	$form.init();
+});
 //#endregion js.ready
 
 
