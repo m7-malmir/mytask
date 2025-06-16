04f99648c2fc2061014b2ad773f4e832237acaf1
m7-malmir <m.malmir666@gmail.com>
2025-06-16 17:21:41 +0330

    some file add

--- testfpp.js
+++ testfpp.js
@@ -1,78 +1,37 @@
-var that = $(this);
-$.qConfirm(that, "آیا از حذف مطمئن هستید؟", function(btn) {
-    if (btn.toUpperCase() === "OK") { // بررسی اینکه کاربر 
-        
-    }
-});
-
-
-	if(CancelCredit=='false'){
-		//اضافه کردن محصول با همان محدودیت اعتبار ینی این txtCreditBalance  خب
-		}else{
-		// اینجا دیگه آزاد باشه هرچی عسقش کشید هزارتا محصول حتی بیشتر بی نهایت اضافه کنه 
-	}
-
-
-
-        var confirmation = confirm("آیا از حذف سمت مطمئن هستید؟");
-    if (confirmation) {
-        var actorId = $(this).closest("tr").find("td").eq(1).text(); 
-        var params = {
-            ActorId: actorId
-        };
-        
-        FormManager.RevokeActorId(params, function(list) {
-            $.alert("حذف سمت کاربر با موفقیت انجام شد.", "", "rtl", function() {
-                hideLoading();
-                
-                // بارگذاری دوباره داده‌ها پس از حذف
-                var PersonnelNO = $("#txtPersonnelNO").val(); // می‌توانید از همین مقدار استفاده کنید
-                loadEmployeeData(PersonnelNO); // فراخوانی تابع بارگذاری داده‌ها
-            });
-        }, function(error) {
-            console.log(error);
-            alert("خطا در ارسال درخواست: " + error);
-        });
-    }
-
-
-
-
-
-    
-var removeBtn = $("<button/>", { title: "حذف" })
-    .css({
-        cursor: "pointer", backgroundColor: "red", color: "white", border: 0,
-        padding: "5px 0px", borderRadius: "50px", lineHeight: "1.2rem", fontSize: "20px", width: "20px",
-        height: "20px", textAlign: "center"
-    }).text("-").on("click", function () {
-        var $removedRow = $(this).closest("tr");
-        var removedProductId = $removedRow.attr("data-id");
-        $('#tblGoods tbody tr').each(function () {
-            var $row = $(this);
-            var rowProductId = $row.find("td").eq(2).text().trim();
-            if (rowProductId === removedProductId) {
-                $row.find('.CHbox').prop("disabled", false).prop("checked", false); // فعال و بدون تیک
-            }
-        });
-
-        $removedRow.remove();
-        updateTotalPrice();
-        checkAddButtonState();
-    });
-
-
-
-    
-
-RemainCredit   txtDiscount   TotalPrice
-
-
-
-    GoodsCode: $(this).find("col[name='GoodsCode']").text(),
-    GoodsName: $(this).find("col[name='GoodsName']").text(),
-    Qty: $(this).find("col[name='Qty']").text(),
-    UnitPrice: $(this).find("col[name='UnitPrice']").text(),
-    AfterDiscountGoodsPrice: $(this).find("col[name='AfterDiscountGoodsPrice']").text()
-
-
+					FormManager.insertGuestDetail(Guests,
+					    function(dataXml) { 
+							
+								WorkflowService.RunWorkflow("ZJM.FDR.FoodGuestsReservation",
+									'<Content><Id>' + pk + '</Id></Content>',
+									true,
+								
+									function(data) {
+					
+										   $.alert("درخواست شما با موفقیت ارسال شد.", "", "rtl", function() {
+											hideLoading();
+											closeWindow({ OK: true, Result: null });
+										});
+									},
+									function(err) {
+										console.error('مشکلی در شروع فرآیند به وجود آمده:', err); // چاپ خطا در کنسول
+										alert('مشکلی در شروع فرآیند به وجود آمده. ' + err);
+										hideLoading();
+									}
+								);
+								myHideLoading();
+								if ($.isFunction(callback)) {
+									callback();
+								}
+											        $.alert("اطلاعات مهمان با موفقیت ثبت شد.","","rtl",function(){	
+									closeWindow({OK:true, Result:null});
+						        });
+						    },
+						    function(error) { // تابع خطا
+						        console.log("خطای برگشتی:", error);
+						        $.alert("عملیات با خطا مواجه شد: " + (error.message || "خطای ناشناخته"), "", "rtl");
+						    }
+						);
+		            myHideLoading();
+		            if ($.isFunction(callback)) {
+		                callback();
+		            }
