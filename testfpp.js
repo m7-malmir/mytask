b713664f7444b0de24887101845fcf606b43cde7
m7-malmir <m.malmir666@gmail.com>
2025-06-18 18:01:17 +0330

    some file add

--- testfpp.js
+++ testfpp.js
@@ -1,37 +1,19 @@
-					FormManager.insertGuestDetail(Guests,
-					    function(dataXml) { 
-							
-								WorkflowService.RunWorkflow("ZJM.FDR.FoodGuestsReservation",
-									'<Content><Id>' + pk + '</Id></Content>',
-									true,
-								
-									function(data) {
-					
-										   $.alert("درخواست شما با موفقیت ارسال شد.", "", "rtl", function() {
-											hideLoading();
-											closeWindow({ OK: true, Result: null });
-										});
-									},
-									function(err) {
-										console.error('مشکلی در شروع فرآیند به وجود آمده:', err); // چاپ خطا در کنسول
-										alert('مشکلی در شروع فرآیند به وجود آمده. ' + err);
-										hideLoading();
-									}
-								);
-								myHideLoading();
-								if ($.isFunction(callback)) {
-									callback();
-								}
-											        $.alert("اطلاعات مهمان با موفقیت ثبت شد.","","rtl",function(){	
-									closeWindow({OK:true, Result:null});
-						        });
-						    },
-						    function(error) { // تابع خطا
-						        console.log("خطای برگشتی:", error);
-						        $.alert("عملیات با خطا مواجه شد: " + (error.message || "خطای ناشناخته"), "", "rtl");
-						    }
-						);
-		            myHideLoading();
-		            if ($.isFunction(callback)) {
-		                callback();
-		            }
+		[UserId]
+      ,[PersonnelCode]
+      ,[AccYear]
+      ,[DiscountPercent]
+      ,[LimitDiscountPercent]
+      ,[Credit]
+      ,[RemainCredit]
+      ,[CancelCredit]
+
+
+
+	  ////////////////////////////
+	   'UserId':UserId, 
+	   'PersonnelCode':PersonnelCode, 
+	   'AccYear':AccYear, 
+	   'DiscountPercent':DiscountPercent, 
+	   'LimitDiscountPercent':LimitDiscountPercent,
+	   'Credit':Credit,
+	   'RemainCredit':RemainCredit
