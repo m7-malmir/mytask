74dce3f6daa03e2e1ac32e9dd8c0bf02182eb9da
m7-malmir <m.malmir666@gmail.com>
2025-08-18 17:25:22 +0330

    work on GiftCreditConfirm

--- GiftCredit/GiftCreditConfirm.js
+++ GiftCredit/GiftCreditConfirm.js
@@ -283,3 +283,137 @@ var FormManager = {
 	//******************************************************************************************************
 };
 //#endregion
+
+//#region btnDecline.js
+$("#btnDecline").click(function(){
+	var that = $(this);
+	var hameshPopup = $(
+		'<div tabindex="1" style="direction:rtl;" class="ui-form">'+
+	     '<label tabindex="-1" style="text-align:right;" class="ui-form-label">لطفا دلیل مخالفت خود را بنویسید.</label>'+
+	    '</div>'
+	);
+	var commentInput = $("<textarea>", {type: "text"}).addClass("comment-input form-control").css({height:"60px","font-size":"8pt",resize:"none"});
+	hameshPopup.append(commentInput);
+	
+	var res = false;
+	
+	hameshPopup.dialog({
+		buttons: [
+			{
+				text: "ثبت",
+				click: function() {
+					showLoading();
+					if($(this).find('.comment-input').val().trim().length > 0){
+					
+						var hameshDescription = $(this).find('.comment-input').val();
+						var params = {
+							'Context': 'رد شد ('+hameshDescription+')',
+							'DocumentId': DocumentId,
+							'CreatorActorId': CurrentUserActorId,
+							'InboxId': InboxId
+						};
+						
+						FormManager.InsertHamesh(params,
+							function(res)
+							{
+								Office.Inbox.setResponse(dialogArguments.WorkItem,0, "",
+								 function(data)
+								    { 
+								        closeWindow({OK:true, Result:null});
+								    }, function(err){ throw Error(err); }
+								);
+							}
+						);
+						
+					}else{
+						$(this).notify('لطفاً علت رد را وارد نمایید',{position:'top'});
+						myHideLoading();
+					}
+				}
+			},
+			{
+				text: "انصراف",
+				click: function(){
+					$(this).dialog("close");
+				}
+			}
+		],
+		open: function( event, ui ) {
+			res = false;
+		},
+		close: function(e,u) {
+			if( res == true ){
+				
+			}
+			else{
+				
+			}
+				
+		}
+	});
+});
+//#endregion
+
+//#region btnAccept.js
+$("#btnAccept").click(function() {
+	// فراخوانی تابع ولیدیشن برای فرم
+    if (!validateIdeaForm()) return;
+	
+	let confirmedGiftCredit = $("#txtConfirmedGiftCredit").val().trim();
+	confirmedGiftCredit = rcommafy ? rcommafy(confirmedGiftCredit) : confirmedGiftCredit.replace(/,/g, "");
+	
+	let list = {ConfirmedGiftCredit: confirmedGiftCredit};
+	list = $.extend(list, { Where: "Id = '" + $form.getPK() + "'" });
+
+		FormManager.updatePersonnelGiftCredit(list,
+	        function(status, list) { 
+				
+				var userId = $("#txtGiftCreditForUserId").val();
+			    var list = {
+			        'UserId': userId,
+			        'EmailText': "<p dir='rtl'>همکار گرامی – اعتبار خرید 100% تخفیف به مبلغ '<b>" + confirmedGiftCredit + "</b>' ريال برای شما در خرید تکی و عمده محصولات لحاظ گردید.</p>",
+			        'EmailSubject': 'اعتبار هدیه'
+			    };
+			    FormManager.SendEmail(list,
+			        function(data) { 
+			            var params = {
+			                'Context': 'تایید شد',
+			                'DocumentId': DocumentId,
+			                'CreatorActorId': CurrentUserActorId,
+			                'InboxId': InboxId
+			            };
+			
+			            FormManager.InsertHamesh(params,
+			                function() { 
+			                    Office.Inbox.setResponse(dialogArguments.WorkItem, 1, "",
+			                        function(data) { 
+			                            // پیام موفقیت قبل از بستن
+			                            showSuccessAlert("اعتبار با موفقیت ثبت و ارسال شد", function(){
+			                                closeWindow({OK:true, Result:null});
+			                            });
+			                        },
+			                        function(err) {
+			                            throw Error(err);
+			                        }
+			                    );
+			                },
+			                function(err) {
+			                    throw Error(err);
+			                }
+			            );
+			      
+						},
+			        function(err) {
+			            throw Error(err);
+			        }
+			    );	
+	        },
+	        function(error) { 
+	            console.log("1خطای برگشتی:", error);
+	            $.alert("عملیات با خطا مواجه شد: " + (error.message || "خطای ناشناخته"), "", "rtl");
+	        }
+		);
+
+});
+
+//#endregion
