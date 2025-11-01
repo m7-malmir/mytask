4337cf6ea93949556098da16d8df48d7c041314c
m7-malmir <m.malmir666@gmail.com>
2025-11-01 09:56:29 +0330

    add webAPI service

---
+++ restSample/RestSamples1.Simple/Program.cs
@@ -0,0 +1,15 @@
+﻿using RestSamples.Model;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ProductDbContext>();
+
+var app = builder.Build();
+
+app.MapGet("/GetProductList", (ProductDbContext dbContext) =>
+    dbContext.Products.ToList()
+);
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
