e60df5a4ff67264dc761ff523b9ef3c636ff3871
m7-malmir <m.malmir666@gmail.com>
2025-12-17 15:28:35 +0330

    work on pricing

--- PRPricing/ZJM.PRI.ChangePricingConfirm.js
+++ PRPricing/ZJM.PRI.ChangePricingConfirm.js
@@ -533,3 +533,158 @@ $("#btnConfirm").click(function () {
   );
 });
 //#endregion btnConfirm.js
+
+//#region Form.DAL.js
+// ===================== Public variables =====================
+var $form;
+var currentActorId;
+var CurrentUserId;
+var isInTestMode = false;
+var primaryKeyName;
+var pk;
+var processStatus;
+$(function () {
+  $form = (function () {
+    var pk,
+      inEditMode = false;
+    // ==================== init ============================
+    function init() {
+      if (typeof dialogArguments !== "undefined") {
+        if (primaryKeyName in dialogArguments) {
+          pk = dialogArguments[primaryKeyName];
+          inEditMode = true;
+          readData();
+        }
+        if ("FormParams" in dialogArguments) {
+          pk = dialogArguments.FormParams;
+          inEditMode = true;
+        }
+        DocumentId = dialogArguments["DocumentId"];
+        CurrentUserActorId = dialogArguments["WorkItem"]["ActorId"];
+        InboxId = dialogArguments["WorkItem"]["InboxId"];
+      }
+      build();
+      createControls();
+    }
+    // ==================== build ============================
+    function build() {
+      //Set the new dialog title
+      changeDialogTitle("اصلاح/تایید مصوبه تغییر قیمت محصولات");
+    }
+    function bindEvents() {}
+    // =============== CreateControls ================
+    function createControls() {
+      UserService.GetCurrentUser(
+        true,
+        function (data) {
+          const xml = $.xmlDOM(data);
+          const userInfo = {
+            id: xml.find("user > id").text().trim(),
+            username: xml.find("user > username").text().trim(),
+            firstname: xml.find("user > firstname").text().trim(),
+            lastname: xml.find("user > lastname").text().trim(),
+            logintime: xml.find("user > logintime").text().trim(),
+            delegatedactorid: xml.find("user > delegatedactorid").text().trim(),
+            theme: xml.find("user > theme").text().trim(),
+            actors: [],
+          };
+
+          xml.find("user > actors > actor").each(function () {
+            const $actor = $(this);
+            const actorInfo = {
+              pk: $actor.attr("pk"),
+              roleid: $actor.find("roleid").text().trim(),
+              rolename: $actor.find("rolename").text().trim(),
+              rolecode: $actor.find("rolecode").text().trim(),
+              isdefault: $actor.find("isdefault").text().trim() === "True",
+            };
+            userInfo.actors.push(actorInfo);
+          });
+
+          const defaultActor = userInfo.actors.find((actor) => actor.isdefault);
+          const roleid = defaultActor ? defaultActor.roleid : "";
+
+          CurrentUserId = `${userInfo.id},${roleid},${userInfo.username}`;
+          CurrentCompanyId = 1;
+
+          let params = {
+            CurrentCompanyId: CurrentCompanyId,
+            CurrentUserId: CurrentUserId,
+            PageSize: 1,
+            PageIndex: 0,
+            SortOrder: [],
+            FilterConditions: [
+              {
+                Column: "Id",
+                Operator: "EqualTo",
+                Value: pk,
+              },
+            ],
+            CustomFilters: {},
+          };
+          tblMain.refresh();
+          FormManager.readPricing(
+            params,
+            function (response) {
+              const list = Array.isArray(response.list)
+                ? response.list
+                : response;
+
+              if (Array.isArray(list) && list.length > 0) {
+                const item = list[0];
+                // پر کردن اینپوت‌ها
+                $("#txtUserCreator").val(item.fullName || "");
+                $("#txtCreatedDate").val(item.createdDateShamsi || "");
+                $("#txtId").val(pk);
+                processStatus = item.processStatus;
+                // نمایش دکمه در کردن برای تسک مدیرعامل
+                if (processStatus === 15) {
+                  $("#btnDecline").show();
+                } else {
+                  $("#btnDecline").hide();
+                }
+
+                if (processStatus === 10) {
+                  $("#btnReview").hide();
+                } else {
+                  $("#btnReview").show();
+                }
+              } else {
+                warningDialog("پیام", "داده‌ای یافت نشد", "rtl");
+              }
+
+              closeLoading();
+            },
+            function (error) {
+              closeLoading();
+              errorDialog("Error", error.message, "rtl");
+            }
+          );
+          /*
+           * After setting the variable 'CurrentUserId',
+           * the table is refreshed by passing its values.
+           */
+          //tblMain.refresh();
+        },
+        function (err) {
+          $ErrorHandling.Erro(err, "خطا در سرویس getCurrentActor");
+          CurrentCompanyId = 1;
+          //tblMain.load();
+        }
+      );
+    }
+
+    // ==================== getPrimaryKey ====================
+    function getPK() {
+      return pk;
+    }
+    // =================== Return ====================
+    return {
+      init: init,
+      getPK: getPK,
+    };
+  })();
+
+  $form.init();
+});
+//#endregion Form.DAL.js
