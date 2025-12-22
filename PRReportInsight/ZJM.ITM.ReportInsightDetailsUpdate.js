562bf25fe2dd98d91764f13614c6605b73581ca3
m7-malmir <m.malmir666@gmail.com>
2025-12-22 17:33:11 +0330

    work on pricing

--- PRReportInsight/ZJM.ITM.ReportInsightDetailsUpdate.js
+++ PRReportInsight/ZJM.ITM.ReportInsightDetailsUpdate.js
@@ -1,3 +1,57 @@
+//#region ready.js
+let $form;
+let CurrentUserId = 0;
+let itemData = {};
+let reportInsightDetailId;
+let reportInsightId;
+let goodsId;
+$(function () {
+  $form = (function () {
+    //=====================Init============================
+    function init() {
+      CurrentUserId = dialogArguments["currentUserId"];
+      itemData.id = dialogArguments["id"];
+      itemData.goodsName = dialogArguments["goodsName"];
+      itemData.reportInsightId = dialogArguments["reportInsightId"];
+      itemData.goodsId = dialogArguments["goodsId"];
+      itemData.willingnessPay = dialogArguments["willingnessPay"];
+      itemData.optimalPricePoint = dialogArguments["optimalPricePoint"];
+      itemData.acceptablePriceRange = dialogArguments["acceptablePriceRange"];
+      itemData.priceElasticityDemand = dialogArguments["priceElasticityDemand"];
+      itemData.samtInfoId = dialogArguments["samtInfoId"];
+
+      build();
+    }
+    function commafy(x) {
+      return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
+    //=====================build============================
+    function build() {
+      changeDialogTitle("جزییات گزارش ترید مارکتینگ");
+      reportInsightDetailId = itemData.id;
+      reportInsightId = itemData.reportInsightId;
+      goodsId = itemData.goodsId;
+      $("#txtGoodsName").val(itemData.goodsName);
+      $("#txtWillingnessPay").val(commafy(itemData.willingnessPay));
+      $("#txtOptimalPricePoint").val(commafy(itemData.optimalPricePoint));
+      $("#txtAcceptablePriceRange").val(commafy(itemData.acceptablePriceRange));
+      $("#txtPriceElasticityDemand").val(
+        commafy(itemData.priceElasticityDemand)
+      );
+      $("#cmbGoodsId").val(itemData.goodsId);
+      $("#lblReportInsightId").text(itemData.reportInsightId);
+    }
+
+    return {
+      init: init,
+    };
+  })();
+
+  $form.init();
+});
+
+//#endregion ready.js
+
 //#region btnUpdate.js
 $("#btnUpdate").click(function () {
   function rcommafy(x) {
